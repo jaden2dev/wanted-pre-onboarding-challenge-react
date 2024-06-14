9307831f6793a6a4a42032f0f3cfdac05fa035a0
homin.ahn <jaden2dev@gmail.com>
2024-06-14 12:20:53 +0900

    feat. chapter3
    image, font

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css'
+import { inter} from './ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}
+      >{children}</body>
     </html>
   );
 }
---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import {Inter, Lobster, Lusitana  } from 'next/font/google'
+
+
+export const inter = Inter({subsets: ['latin']})
+export const lobster = Lobster({ weight: '400',  subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
